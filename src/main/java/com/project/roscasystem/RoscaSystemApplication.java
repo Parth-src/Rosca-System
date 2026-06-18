fae7baeed05e915f0d2b489df025bbf511a847ad
Parth-src <parth.prog01@gmail.com>
2026-06-18 17:42:19 +0530

    packages created

---
+++ src/main/java/com/project/roscasystem/RoscaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.roscasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoscaSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoscaSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/roscasystem/RoscaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.roscasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoscaSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
